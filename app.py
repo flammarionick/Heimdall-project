019f75cd6ec4150807a9b98fed725a21454a0830
FLAMMARYON <nicholaseke04@gmail.com>
2024-11-21 07:38:09 +0200

    deploy

--- app.py
+++ app.py
@@ -579,4 +579,7 @@ def get_user_by_username(username):
 if __name__ == '__main__':
     init_db()  # This should create the 'users' table and any others if not already created
     verify_or_add_admin()  # Now we can safely verify or add the default admin
-    app.run(debug=True)
+
+    # Use the environment variable PORT if available, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
